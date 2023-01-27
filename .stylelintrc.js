8c975fd760c5e010329dd9dc8d2cebdf6220b3dc
Predrag Nastic <jazzerapn@gmail.com>
2023-01-27 12:45:57 +0100

    linters added

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": ["stylelint-config-standard-scss"],
+  "plugins": ["stylelint-scss"],
+  "rules": {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "no-descending-specificity": null,
+    "indentation": 2,
+    "scss/dollar-variable-pattern": [/[a-z][a-zA-Z]+/], // Prevent: Expected variable to be kebab-case  scss/dollar-variable-pattern
+    "selector-class-pattern": null,
+  },
+  "ignoreFiles": []
+}
